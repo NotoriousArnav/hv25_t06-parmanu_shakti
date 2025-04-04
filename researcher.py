a91946f8368168aa235dae98277dab65f8705ceb
NotoriousArnav <arnavghosh.official.2004@gmail.com>
2025-04-04 15:58:13 +0530

    Added Streamlit

---
+++ researcher.py
@@ -0,0 +1,46 @@
+os.environ['GROQ_API_KEY'] = "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"
+os.environ['TAVILY_API_KEY'] = "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"
+
+#!pip install open-deep-research
+
+from langgraph.checkpoint.memory import MemorySaver
+from open_deep_research.graph import builder
+memory = MemorySaver()
+graph = builder.compile(checkpointer=memory)
+
+import uuid
+thread = {"configurable": {
+                            "thread_id": str(uuid.uuid4()),
+                            "search_api": "tavily",
+                            "planner_provider": "groq",
+                            "planner_model": "llama-3.3-70b-versatile",
+                            "writer_provider": "groq",
+                            "writer_model": "llama-3.3-70b-versatile",
+                            "max_search_depth": 3,
+                           }
+          }
+
+# @title Enter your Research Topic
+# @markdown Open research will use Tavily to deep Research about the Topic
+topic = "Is it true that Muslims will be charged a New Tax from September of 2025 in India?" # @param {"type":"string"}
+async for event in graph.astream({"topic":topic,}, thread, stream_mode="updates"):
+    print(event)
+
+print(event['__interrupt__'][0].value)
+
+from langgraph.types import Command
+async for event in graph.astream(
+        Command(
+resume = True # @param {"type":"raw"}
+        ),
+        thread,
+        stream_mode="updates"
+    ):
+    ...
+print(event['compile_final_report']['final_report'])
+
+async for event in graph.astream(Command(resume=True), thread, stream_mode="updates"):
+    print(event)
+
+from IPython.display import display, Markdown
+display(Markdown(event['compile_final_report']['final_report']))
